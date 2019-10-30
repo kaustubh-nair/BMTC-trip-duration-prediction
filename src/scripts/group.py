3ba02a6da1d2002b63f1b147e239d67f5d7fd6bf
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-10-30 14:26:17 +0530

    add sort.sh

--- src/scripts/group.py
+++ src/scripts/group.py
@@ -1,8 +1,17 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'dataset/small.csv'), chunksize=300000)
+df = pd.read_csv('../dataset/medium.csv', chunksize=300000)
 
-for i in df:
-    print(i)
+buses = np.array([])
+for data in df:
+    buses = np.append(buses, data.BusID.unique())
+    sizes = []
+    print(np.unique(buses).size)
+    for bus in buses:
+        rows = data.loc[data['BusID'] == bus]
+        sizes.append(rows.size)
+
+print(np.unique(buses).size)
