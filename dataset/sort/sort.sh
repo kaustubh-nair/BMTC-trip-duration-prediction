05410951cb90904dbfdf420ef29c347f4edd5bd6
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-10-30 17:22:09 +0530

    Modify .gitignore

--- dataset/sort/sort.sh
+++ dataset/sort/sort.sh
@@ -1,4 +1,6 @@
-split -b250M ../w1.csv fileChunk
-for f in fileChunk*; do sort -t, --parallel=4 -k1,1n -k6,6 -s "$f" > "$f".sorted && rm "$f"; done
-sort -T temp/ --parallel=4 -mo w1_sorted.csv -k1,1 -k6,6 --stable fileChunk*.sorted
-rm fileChunk*
+# sort the dataset based on the first and sixth  columns
+
+split -b250M ../small.csv chunk
+for f in chunk*; do sort -t, --parallel=4 -k1,1n -k6,6 -s "$f" > "$f".sorted && rm "$f"; done
+sort -T temp/ --parallel=4 -mo ../w1_sorted.csv -k1,1 -k6,6 --stable chunk*.sorted
+rm chunk*
