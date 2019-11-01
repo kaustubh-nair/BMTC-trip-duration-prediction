e92f3b0ceee24b63fc11c179f769c47b29477ac7
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-11-01 19:07:56 +0530

    Sort dataset

--- dataset/sort/sort.sh
+++ dataset/sort/sort.sh
@@ -1,6 +1,2 @@
-# sort the dataset based on the first and sixth  columns
-
-split -b250M ../small.csv chunk
-for f in chunk*; do sort -t, --parallel=4 -k1,1n -k6,6 -s "$f" > "$f".sorted && rm "$f"; done
-sort -T temp/ --parallel=4 -mo ../w1_sorted.csv -k1,1 -k6,6 --stable chunk*.sorted
-rm chunk*
+#&& rm "$f"; done sort the dataset based on the first and sixth  columns
+sort -t, -k1,1n -k6,6 -s ../w1.csv > ../w1.csv.sorted 
