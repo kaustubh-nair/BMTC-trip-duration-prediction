46d92e3310d527e81453a2c4fe26b3d152a8c5dc
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-11-01 19:54:11 +0530

    Remove zero

--- src/scripts/outlier.py
+++ src/scripts/outlier.py
@@ -3,11 +3,11 @@ import numpy as np
 
 zerovalues = [0, 0, 0]
 
-df = pd.read_csv("../dataset/small.csv.sorted")
-zerocolumns = ["BusID", "LAT", "LON"]
-for j in range(len(df)):
-    for k in range(len(zerovalues)):
-        if(np.equal(df.loc[j, zerocolumns[k]], 0)):
-            zerovalues[k] += 1
+df = pd.read_csv("../dataset/small.csv.sorted", chunksize=1000)
 
-print(zerovalues)
+for chunk in df:
+    print(chunk.size)
+    df_filtered = chunk[chunk['LAT'] != 0]
+    df_filtered = df_filtered[df_filtered['LON'] != 0]
+    print(df_filtered.size)
+    df_filtered.to_csv("../dataset/small.csv",mode='a',index=False)
