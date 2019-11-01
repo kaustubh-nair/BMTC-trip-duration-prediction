78a8601824de33c406689a7c89ae68059ed31124
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-11-01 10:26:47 +0530

    Fix syntax

--- src/visualisation/vis.py
+++ src/visualisation/vis.py
@@ -6,11 +6,11 @@ dtypes = {'BusID': np.int32, 'LAT': np.float32, 'LON': np.float32, 'Angle': np.f
 df = pd.read_csv('../dataset/small_sorted.csv', dtype=dtypes)
 
 i = 0
-mapit = folium.Map( location=[12.9716, 77.5946], zoom_start=12 ,prefer_canvas=True)
+mapit = folium.Map(location=[12.9716, 77.5946], zoom_start=12, prefer_canvas=True)
 for index, row in df.iterrows():
     if i == 10000:
         break
     print(row['LAT'], row['LON'])
-    folium.CircleMarker( location=[ row['LAT'], row['LON'] ], fill_color='#43d9de', radius=2,weight=2 ).add_to( mapit )
-    i+=1
-mapit.save( 'map.html')
+    folium.CircleMarker(location=[row['LAT'], row['LON']], fill_color='#43d9de', radius=2, weight=2).add_to(mapit)
+    i += 1
+mapit.save('map.html')
