ebf8aff7c8eaf3b36f417823ccdc6e959343b560
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-11-02 11:16:51 +0530

    Fix outliers

--- src/scripts/outlier.py
+++ src/scripts/outlier.py
@@ -3,11 +3,10 @@ import numpy as np
 
 zerovalues = [0, 0, 0]
 
-df = pd.read_csv("../dataset/small.csv", chunksize=1000)
+df = pd.read_csv("../dataset/small.csv", chunksize=3000000)
 
 for chunk in df:
-    print(chunk.size)
     df_filtered = chunk[chunk['LAT'] != 0]
     df_filtered = df_filtered[df_filtered['LON'] != 0]
     print(df_filtered.size)
-    df_filtered.to_csv("../dataset/small.csv",mode='a',index=False)
+    df_filtered.to_csv("../dataset/w1_nonzero.csv",mode='a',index=False,header=False)
