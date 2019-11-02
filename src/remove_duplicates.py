93c1eeb0ea3538a1233bdeaf209f9f8ec7a29dd0
Ayush Yadav <ayushyadav.ay42@gmail.com>
2019-11-02 11:24:56 +0530

    added data cleaning code

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 large_frame = pd.read_csv("../dataset/w1.csv", chunks=10000000)
-count = 0
 with open("../dataset/cleaned_w1.csv", "a") as f:
     for df in large_frame:
-        print (count)
         df['unique'] = pd.Series(0, index=df.index)
         unique_rows = df.drop_duplicates(subset=['BusID', 'LAT', 'LON', 'Angle', 'Speed'])
 
@@ -18,5 +16,5 @@ with open("../dataset/cleaned_w1.csv", "a") as f:
             df.set_value(index, 'unique', 1)
 
         df.drop(df[df['unique'] == 0].index, inplace=True)
+        print (df.size)
         df.to_csv(f, header=False, index=False)
-        count += 1
