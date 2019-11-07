43b32a789bd2787209944dcaaaa3e086f4f080e6
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-11-07 16:46:46 +0530

    Make some sick graphs

---
+++ src/scripts/format_dataset.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+dtypes = {'BusID': np.int32, 'LAT': np.float64, 'LON': np.float64, 'Angle': np.int32, 'Speed': np.int32, 'Timestamp': 'str'}
+large_frame = pd.read_csv("../dataset/small.csv", chunksize=3000000,dtype=dtypes)
+with open("../dataset/formatted_small.csv", "a") as f:
+
+    f.write("BusID,LAT,LON,Angle,Speed,Timestamp,\n")
+    f.write("BusID,LAT,LON,Angle,Speed,Timestamp,year,month,day,hour,minute,second\n")
+    for df in large_frame:
+        df['unique'] = pd.Series(0, index=df.index)
+        unique_rows = df.drop_duplicates(subset=['BusID', 'LAT', 'LON', 'Angle', 'Speed'])
+
+        indices = []
+        for i,j in unique_rows.iterrows():
+            indices.append(i)
+            if (i > 0):
+                indices.append(i-1)    
+        for index in indices:
+            df.set_value(index, 'unique', 1)
+
+        df.drop(df[df['unique'] == 0].index, inplace=True)
+        print (df.size)
+        df.to_csv(f, header=False, index=False,float_format="%.6f")
--- src/visualisation/vis_test.py
+++ src/visualisation/vis_test.py
@@ -8,16 +8,17 @@ i = 0
 mapit = folium.Map(location=[12.9716, 77.5946], zoom_start=12, prefer_canvas=True)
 print(df)
 for index, row in df.iterrows():
+    if(i==7):
+        break
     #print(row['LAT'], row['LON'])
     j = 1
     while j < 101:
-        print("ASD")
         lat = row["LATLONG"+str(j)].split(":")[0]
         lon = row["LATLONG"+str(j)].split(":")[1]
-        print(lat,lon)
-        folium.CircleMarker(location=[lat,lon], fill_color='#43d9de', radius=2, weight=2).add_to(mapit)
+        print(lat,lon,row["Id"])
+        # red black grey cyan blue pink
+        bus = {150220445: "#eb4034", 150811705:"#000d09",150218073:"#e2eb34",150218420:"#7d7d7d",150219092:"#34ebeb",150814169:"#345ceb",150220781:"#d934eb"}
+        folium.CircleMarker(location=[lat,lon], color=bus[row["Id"]], radius=2, weight=2).add_to(mapit)
         j+=1
     i += 1
-    if(i==3):
-        break
-mapit.save('a.html')
+mapit.save('first_six_buses_test.html')
