5a0f472a9c56ff32649a0d583f3f9c8f72c551b3
Ayush Yadav <ayushyadav.ay42@gmail.com>
2019-11-07 16:19:04 +0530

    split_date_time added

--- src/scripts/remove_duplicates.py
+++ src/scripts/remove_duplicates.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-dtypes = {'BusID': np.int32, 'LAT': np.float64, 'LON': np.float64, 'Angle': np.int32, 'Speed': np.int32, 'Timestamp': 'str'}
-large_frame = pd.read_csv("../dataset/w1.csv", chunksize=3000000,dtype=dtypes)
-with open("../dataset/cleaned_w1.csv", "a") as f:
+# dtypes = {'BusID': np.int32, 'LAT': np.float64, 'LON': np.float64, 'Angle': np.int32, 'Speed': np.int32, 'Timestamp': 'str'}
+large_frame = pd.read_csv("../../dataset/Sorted/correct_resuable_w1.csv", chunksize=7000000)
+with open("../../dataset/Sorted/2_cleaned_w1.csv", "a") as f:
 
-    f.write("BusID,LAT,LON,Angle,Speed,Timestamp\n")
+    # f.write("BusID,LAT,LON,Angle,Speed,Timestamp\n")
     for df in large_frame:
         df['unique'] = pd.Series(0, index=df.index)
         unique_rows = df.drop_duplicates(subset=['BusID', 'LAT', 'LON', 'Angle', 'Speed'])
@@ -20,4 +20,4 @@ with open("../dataset/cleaned_w1.csv", "a") as f:
 
         df.drop(df[df['unique'] == 0].index, inplace=True)
         print (df.size)
-        df.to_csv(f, header=False, index=False,float_format="%.6f")
+        df.to_csv(f, header=False, index=False)
--- src/scripts/split_datetime.py
+++ src/scripts/split_datetime.py
@@ -2,17 +2,19 @@ import pandas as pd
 import numpy as np
 
 dtypes = {'BusID': np.int32, 'LAT': np.float64, 'LON': np.float64, 'Angle': np.int32, 'Speed': np.int32, 'Timestamp': 'str'}
-df = pd.read_csv("../dataset/w1.csv", chunksize=3000000,dtype=dtypes)
+df = pd.read_csv("../../dataset/cleaned_small_sorted.csv", chunksize=3000000,dtype=dtypes)
 
 
-with open("../dataset/w1_split_datetime.csv", "a") as f:
-    f.write("BusID,LAT,LON,Angle,Speed,Timestamp,uniquecolumn,year,month,day,hour,minute,second\n")
+with open("../../dataset/w1_split_datetime_small.csv", "a") as f:
+    # f.write("BusID,LAT,LON,Angle,Speed,Timestamp,uniquecolumn,Day,Time\n")
 
     for chunk in df:
-        chunk["Year"] = chunk["Timestamp"].astype(str).str[0:4]
-        chunk["Month"] = chunk["Timestamp"].astype(str).str[5:7]
-        chunk["Day"] = chunk["Timestamp"].astype(str).str[8:10]
-        chunk["Hour"] = chunk["Timestamp"].astype(str).str[11:13]
-        chunk["Minute"] = chunk["Timestamp"].astype(str).str[14:16]
-        chunk["Second"] = chunk["Timestamp"].astype(str).str[17:19]
+        chunk.columns = ['BusID','LAT', 'LON', 'Angle', 'Speed', 'Timestamp','uniquecol']
+        # chunk["Year"] = chunk["Timestamp"].astype(str).str[0:4]
+        # chunk["Month"] = chunk["Timestamp"].astype(str).str[5:7]
+        chunk["Day"] = (chunk["Timestamp"].astype(str).str[8:10]).astype(int)
+        
+        chunk["Time"] = (chunk["Timestamp"].astype(str).str[11:13]).astype(int) + 10**-2*(chunk["Timestamp"].astype(str).str[14:16]).astype(int) + 10**-4*chunk["Timestamp"].astype(str).str[17:19]
+        # chunk["Minute"] = chunk["Timestamp"].astype(str).str[14:16]
+        # chunk["Second"] = chunk["Timestamp"].astype(str).str[17:19]
         chunk.to_csv(f, header=False, index=False,float_format="%.6f")
