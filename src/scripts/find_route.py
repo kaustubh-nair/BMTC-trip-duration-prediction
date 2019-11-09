5f98b65723111c24dcc9d041913f84f4a37c9f33
Ayush Yadav <ayushyadav.ay42@gmail.com>
2019-11-09 16:48:32 +0530

    another route added

--- src/scripts/find_route.py
+++ src/scripts/find_route.py
@@ -3,9 +3,9 @@ import numpy as np
 
 large_frame = pd.read_csv("../../dataset/Sorted/cleaned_reusable_w1.csv", chunksize=7000000)
 save = []
-with open("../../dataset/route_150811705.csv", "a") as f:
+with open("../../dataset/route_150220445.csv", "a") as f:
     for df in large_frame:
         df.columns = ['BusID','LAT', 'LON', 'Angle', 'Speed', 'Timestamp']
-        a = df.loc[df['BusID'] == 150811705]
+        a = df.loc[df['BusID'] == 150220445]
         a.to_csv(f, index=False)        
     
