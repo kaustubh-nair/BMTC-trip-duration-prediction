4eee5ee8be3f81fa302ccc89a4fb671f65946408
Ayush Yadav <ayushyadav.ay42@gmail.com>
2019-11-17 14:47:16 +0530

    made tuples

--- src/scripts/pairwise.py
+++ src/scripts/pairwise.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import math
-
-df = pd.read_csv("cleanset.csv")
+import sys
+large = pd.read_csv("../../dataset/Sorted/removed_duplicates/cleaned_w1.csv", chunksize=1000000)
 
 def angleFromCoordinate(LAT1, LON1, LAT2, LON2):
     dLon = (LON2 - LON1)
@@ -15,7 +15,7 @@ def angleFromCoordinate(LAT1, LON1, LAT2, LON2):
     return brng
 
 def calculateDirection(angle):
-    if(angle <=45 and angle > 315):
+    if(angle <=45 or angle > 315):
         return 'E'
     if(angle >45 and angle <= 135):
         return 'N'
@@ -24,15 +24,23 @@ def calculateDirection(angle):
     if(angle >225 and angle <= 315):
         return 'S'
 
-for i in range(len(df)-1):
-    df.at[i, "LAT2"] = df.loc[i+1, "LAT1"]
-    df.at[i, "LON2"] = df.loc[i+1, "LON1"]
-    df.at[i, "Timestamp2"] = df.loc[i+1, "Timestamp1"]
-    df.at[i, "Angle"] = angleFromCoordinate(df.loc[i, "LAT1"], df.loc[i, "LON1"], df.loc[i, "LAT2"], df.loc[i, "LON2"])
-    df.at[i, "Direction"] = calculateDirection(df.loc[i, "Angle"])
-
-df = df.head(len(df)-1)
-
-with open("pairwise_dataset.csv", "a") as f:
+cs = 1000000
+j = 0
+with open("../../dataset/Sorted/removed_duplicates/pairwise_dataset.csv", "a") as f:
+    
     f.write("BusID,LAT1,LON1,Timestamp1,LAT2,LON2,Timestamp2,Angle,Direction\n")
-    df.to_csv(f, header=False, index=False,float_format="%.6f")
+    for df in large:
+        print(df.dtypes)
+        df.columns = ["BusID", "LAT1", "LON1","Timestamp1"]
+        for i in range(len(df)-1):
+            df.at[i + j*cs, "LAT2"] = df.loc[i+j*cs +1, "LAT1"]
+            df.at[i + j*cs, "LON2"] = df.loc[i+ + j*cs + 1, "LON1"]
+            df.at[i + j*cs, "Timestamp2"] = df.loc[i + j*cs +1, "Timestamp1"]
+            df.at[i + j*cs, "Angle"] = angleFromCoordinate(df.loc[i + j*cs, "LAT1"], df.loc[i + j*cs, "LON1"], df.loc[i + j*cs, "LAT2"], df.loc[i + j*cs, "LON2"])
+            df.at[i + j*cs, "Direction"] = calculateDirection(df.loc[i+j*cs, "Angle"])
+        #df = df.head(len(df)-1)
+        df.drop(df.index[len(df)-1], inplace=True)
+        df.to_csv(f, header=False, index=False,float_format="%.6f")
+        print (j)
+        j += 1
+
